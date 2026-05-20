c974b7a9a1adec813d9b01f94d58b139107519bb
Psychovv <viniciusmedina63@gmail.com>
2026-05-20 16:47:54 -0300

    feat: register Fotografia model with custom admin display

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Fotografia
 
-# Register your models here.
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ('nome', 'legenda', 'descricao', 'foto')
+
+admin.site.register(Fotografia, ListandoFotografias)
+
