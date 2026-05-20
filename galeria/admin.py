b62f8e6dcb4650901632bbf2053db3399bda8961
Psychovv <viniciusmedina63@gmail.com>
2026-05-20 16:59:50 -0300

    feat: personalizando admin para o app galeria

--- galeria/admin.py
+++ galeria/admin.py
@@ -3,6 +3,8 @@ from .models import Fotografia
 
 class ListandoFotografias(admin.ModelAdmin):
     list_display = ('nome', 'legenda', 'descricao', 'foto')
+    list_filter = ('categoria',)
+    list_per_page = 10
 
 admin.site.register(Fotografia, ListandoFotografias)
 
